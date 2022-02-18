fc92711b2d314a1bf9b95ad5b60f13bf8d975e6c
Cherryl - Kave Blocks <cherryl@kaveblocks.com>
2022-02-18 16:24:37 +0800

    additional images

--- exer.js
+++ exer.js
@@ -1,12 +1,26 @@
-var image = new Array(
-  "assets/1.jpg",
-  "assets/2.jpg",
-  "assets/3.png",
-  "assets/4.png",
-  "assets/5.jpg",
-  "assets/6.png",
-  "assets/7.jpg"
-);
+// get all the images at assests folder
+var image = [
+  '1.jpg',
+  '2.jpg',
+  '3.png',
+  '4.png',
+  '5.jpg',
+  '6.png',
+  '7.jpg'
+];
 
+//Call the onClick button named RUN
 function run() {
+
+  //random index
+  random_index = Math.floor(Math.random() * image);
+
+
+  //get an image at random index
+   selected_image = image[random_index];
+
+
+   document.getElementById('display_image').src = `./assets/${selected_image }`;
+
+
 }
